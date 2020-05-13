ec4ef9f89543fdada18a6bc2afe1948f0c910e39
sviney <sviney@users.noreply.github.com>
2020-05-13 10:13:26 +0100

    class Phone added

---
+++ ShannonViney_S00186271/Phone.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShannonViney_S00186271
+{
+    public class Phone
+    {
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public string OperatingSystem { get; set; }
+        public string OS_Image { get; set; }
+        public string Phone_Image { get; set; }
+    }
+}
